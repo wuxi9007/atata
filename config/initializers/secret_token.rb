9685cd85b32dbf731832dfd401c55d36ff31a8e9
wuxi9007 <wuxi9007@gmail.com>
2015-02-10 15:04:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccelerometerData::Application.config.secret_key_base = 'caf9a4d545e5b0701ab43152399e926669fec5c00811f0b232a4bda35b8f0df9c5c7b6afd7fa80e3462025e5f009eb138da29074ec0c38d5f290d2a0665bd25a'
