a63776e9ac6dac959c0c05bc9683d3e8a68ce6d9
wuxi9007 <wuxi9007@gmail.com>
2015-02-27 16:32:44 -0500

    parse

--- app/models/acc_datum.rb
+++ app/models/acc_datum.rb
@@ -13,11 +13,23 @@ def self.to_csv
 
 def self.import(file)
   CSV.foreach(file.path, headers: true) do |row|
-    AccDatum.create! row.to_hash
+    row_hash = row.to_s
+    android_id = row_hash.split(",")[0]
+    time_stamp = row_hash.split(",")[1]
+    x = row_hash.split(",")[2]
+    y = row_hash.split(",")[3]
+    z = row_hash.split(",")[4]
+    notes = row_hash.split(",")[5]
+    accdatum = AccDatum.new(
+      android_id: android_id,
+      time_stamp: time_stamp,
+      x: x,
+      y: y,
+      z: z,
+      notes: notes
+      )
+    accdatum.save!
   end
 end
-
-
-
    
 end
